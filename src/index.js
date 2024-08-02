eff8d41f2ee08978eff4b7d04f0cff23a89a9d4c
king-dato <king_dato@yahoo.com>
2024-08-01 23:55:56 -0400

    update hw 7

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { App } from 'components/App/App';
+import { GlobalStyle } from './components/styleTheme/GlobalStyle';
+import { theme } from './components/styleTheme/theme';
+import { store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-07-phonebook">
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
